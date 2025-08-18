737d396a185f168c1dd7b3f63d6810f1955749ae
John Currie <johnmcurrie@hotmail.com>
2025-08-17 20:26:17 -0500

    updating files for PWA

--- sw.js
+++ sw.js
@@ -1,16 +1,18 @@
-const CACHE_NAME = 'pf-circuit-cache-v1';
+const CACHE_NAME = "pf-circuit-cache-v1";
 const urlsToCache = [
-  './index.html',
-  './manifest.json'
+  "./index.html",
+  "./manifest.json",
+  "./sw.js"
+  // add any other files you reference, e.g., CSS or JS
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => response || fetch(event.request))
   );
